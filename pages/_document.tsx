20d27e92624907295e0ef28009463bfc24f3d481
hungdoba <hungdoba.hdb@gmail.com>
2024-03-19 22:01:05 +0900

    update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,12 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
+import i18nextConfig from '../next-i18next.config';
 
 export default function Document() {
+  const currentLocale = i18nextConfig.i18n.defaultLocale;
   return (
-    <Html lang="en">
+    <Html lang={currentLocale} className="scroll-smooth scroll-pt-28">
       <Head />
-      <body>
+      <body className="bg-white text-gray-600 work-sans leading-normal text-base tracking-normal">
         <Main />
         <NextScript />
       </body>
