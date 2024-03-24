d1fa1ce810f5ede2fcef3ab66cb06b57cfed2fd2
hungdoba <hungdoba.hdb@gmail.com>
2024-03-24 18:26:29 +0900

    add vercel speedinsights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { SessionProvider } from 'next-auth/react';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </SessionProvider>
   );
 };
