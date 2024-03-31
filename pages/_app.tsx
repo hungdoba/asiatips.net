3feea53fbcc839dfddd56f69a80460f45e64328d
hungdoba <hungdoba.hdb@gmail.com>
2024-03-31 20:58:20 +0900

    rm analytic and speeed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,15 +3,15 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import { SessionProvider } from 'next-auth/react';
-import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
+// import { Analytics } from '@vercel/analytics/react';
+// import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
-      <Analytics />
-      <SpeedInsights />
+      {/* <Analytics /> */}
+      {/* <SpeedInsights /> */}
     </SessionProvider>
   );
 };
